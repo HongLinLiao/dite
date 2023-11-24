3833e22c87cda318e2e558adbda2e3cb485988dd
Leo Liao <honglin0822@outlook.com>
2023-11-24 16:44:36 +0800

    :sparkles: Add dockerfile

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,12 +1,5 @@
-import path from "path";
 import * as dotenv from "dotenv";
-
-const NODE_ENV = process.env.NODE_ENV;
-dotenv.config({
-  path: NODE_ENV
-    ? path.resolve(__dirname, `../../.env.${NODE_ENV}`)
-    : path.resolve(__dirname, "../../.env"),
-});
+dotenv.config();
 
 const env: Env = {
   environment: process.env.ENV as Environment,
